c8477396e30ddbfe2903cca84589f91da235f64c
Rémy Sanchez <remy.sanchez@hyperthese.net>
2014-09-02 13:34:12 +0200

    Static files serving

--- activeseed/urls.py
+++ activeseed/urls.py
@@ -4,10 +4,12 @@
 # (c) 2014 ActivKonnect
 
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 
 urlpatterns = patterns(
     '',
 
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
